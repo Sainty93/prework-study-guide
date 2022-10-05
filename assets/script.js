f3022862709ad2171fce87f9b39f1bd8590f41f6
Sainty93 <jenandfloyd93@icloud.com>
2022-10-04 20:16:40 -0700

    added js

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,20 @@
 var topics = ['HTML', 'CSS', 'GIT', 'JavaScript'];
-for (var x = ; x < topics.length; x++) { console.log(topics[x])}
-/*
-if (topic === 'HTML') {
+
+for (var x = ; x < topics.length; x++) {
+  console.log(topics[x]);
+ }
+ 
+ /*
+ if (topic === 'HTML') {
   console.log("Let's study HTML!");
-} else if (topic === 'CSS') {
+ } else if (topic === 'CSS') {
   console.log("Let's study CSS!");
-} else if (topic === 'Git') {
+ } else if (topic === 'Git') {
   console.log("Let's study Git!");
-} else if (topic === 'JavaScript') {
+ } else if (topic === 'JavaScript') {
   console.log("Let's study JavaScript!");
-} else {
+ } else {
   console.log('Please try again!');
-}
-*/
+ }
+ */
+ 
